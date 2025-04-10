66751b0cbb39653d0d64cff1886caad7f8d9a1d3
Gustavo Lopes <gustavo_figueira@outlook.com>
2025-04-10 01:29:44 -0300

    fix: (google) add firebase files

--- lib/app.dart
+++ lib/app.dart
@@ -1,33 +1,91 @@
+import 'dart:async';
+
+import 'package:catcher_2/catcher_2.dart';
+import 'package:dynamic_path_url_strategy/dynamic_path_url_strategy.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:weather_map/app/config/flavors.dart';
+import 'package:weather_map/src/bindings/initial_binding.dart';
+import 'package:weather_map/src/core/constants/locale.constants.dart';
+import 'package:weather_map/src/core/constants/theme.dart';
+import 'package:weather_map/src/core/services/navigation/navigation_service.dart';
+import 'package:weather_map/src/core/services/services.dart';
+import 'package:weather_map/src/views/home_view.dart';
 
-import 'flavors.dart';
-import 'pages/my_home_page.dart';
+const defaultTranslationsPath = 'assets/translations';
 
-class App extends StatelessWidget {
-  const App({super.key});
+void _runApp({required MainAppEnvironment environment}) => runApp(
+  EasyLocalization(
+    path: defaultTranslationsPath,
+    supportedLocales: supportedLocales,
+    fallbackLocale: defaultLocale,
+    child: GetMaterialApp(
+      title: 'Mobile Challenge (ProjectMark)',
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.lightTheme,
+    ),
+  ),
+);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _flavorBanner(child: MyHomePage(), show: kDebugMode),
-    );
-  }
+Future<void> mainApp({required MainAppEnvironment environment}) async {
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await EasyLocalization.ensureInitialized();
 
-  Widget _flavorBanner({required Widget child, bool show = true}) => show
-      ? Banner(
-          location: BannerLocation.topStart,
-          message: F.name,
-          color: Colors.green.withAlpha(150),
-          textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0,
-          ),
-          textDirection: TextDirection.ltr,
-          child: child,
-        )
-      : Container(child: child);
+      // Remove the # symbol from the URL (web only).
+      setPathUrlStrategy();
+
+      // Make status bar translucent.
+      SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      );
+
+      // Init services of the app .
+      initServices(environment);
+
+      // Initialize the bindings before running the app.
+      InitialBinding().dependencies();
+
+      if (kDebugMode) {
+        _runApp(environment: environment);
+      } else {
+        Catcher2Options debugOptions = Catcher2Options(DialogReportMode(), [
+          ConsoleHandler(),
+        ]);
+        Catcher2Options releaseOptions = Catcher2Options(DialogReportMode(), [
+          EmailManualHandler(["recipient@email.com"]),
+        ]);
+        // Catcher2Options profileOptions = Catcher2Options(
+        //   NotificationReportMode(), [ConsoleHandler(), ToastHandler()],
+        //   handlerTimeout: 10000, customParameters: {"example"c: "example_parameter"},);
+
+        Catcher2(
+          runAppFunction: () => _runApp(environment: environment),
+          navigatorKey: NavigationService.rootNavigatorKey,
+          debugConfig: debugOptions,
+          releaseConfig: releaseOptions,
+          //profileConfig: profileOptions,
+          // enableLogger: false,
+        );
+      }
+    },
+    (error, stack) {
+      FlutterError.reportError(
+        FlutterErrorDetails(
+          exception: error,
+          stack: stack,
+          context: ErrorDescription('runZonedGuarded - uncaught error'),
+        ),
+      );
+    },
+  );
 }
